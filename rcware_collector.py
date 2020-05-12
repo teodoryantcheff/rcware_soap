278b031b8a1e08c6966e24b23b1c31df67c1b774
Teodor Yantcheff <teodor.yantchev@gmail.com>
2020-05-12 11:19:02 +0300

    something working

--- rcware_collector.py
+++ rcware_collector.py
@@ -3,12 +3,13 @@ import time
 from datetime import datetime, timedelta
 
 import zeep
+from influxdb import InfluxDBClient
 
 from credentials import username, password
 
 # logging.basicConfig(level=logging.DEBUG)
 
-RCW_VARIABLE_COUNT = 3000
+RCW_GETDATA_VALUE_COUNT = 3000
 WSDL = 'wsdl/RcWareDbAccess.wsdl'
 
 settings = zeep.Settings(strict=True, xml_huge_tree=True, force_https=False, raw_response=False)
@@ -19,6 +20,8 @@ client = zeep.Client(wsdl=WSDL, settings=settings, port_name='HistoryAccess')
 factory = client.type_factory('http://schemas.datacontract.org/2004/07/ESG.Db.Server.Shared')
 
 creds = factory.Credentials(Name=username, Password=password)
+
+influxdb = InfluxDBClient('influxdb.power-hash.com', port=80)
 
 
 def load_mapping(filename):
@@ -35,13 +38,13 @@ def load_mapping(filename):
 def rcw_to_influx(mvr, rcw_mapping):
     # (StationName: DPGuid) tuple used as key in the config dictionary
     lookup = (mvr['Keys']['KeyValuePair'][5]['Value'], mvr['Keys']['KeyValuePair'][1]['Value'])
+    # lookup = tuple([k['Value'] for k in mvr['Keys']['KeyValuePair'] if k['Key'] in ['DPGuid', 'StationName']])  # todo this can be indices
 
     datapoints = []
 
-    # lookup = tuple([k['Value'] for k in mvr['Keys']['KeyValuePair'] if k['Key'] in ['DPGuid', 'StationName']])  # todo this can be indices
     for value in mvr['Vals']['I']:
         ts = value['Gt']
-        value = value['Dv']
+        val = value['Dv']
 
         dp_dict = rcw_mapping[lookup]
 
@@ -50,7 +53,7 @@ def rcw_to_influx(mvr, rcw_mapping):
             "tags": {tag: dp_dict[tag] for tag in ["device", "plant", "device_type"]},
             "time": ts,
             "fields": {
-                "value": float(value)
+                "value": float(val)
             }
         }
         datapoints.append(data_point)
@@ -60,18 +63,18 @@ def rcw_to_influx(mvr, rcw_mapping):
 def read_rcware_measurements(rcw_mapping: dict, since: datetime, to=datetime.utcnow()):
     """
 
-    :param to: UTC datetime to read data to. Default: utcnow()
     :param rcw_mapping: metrics mapping between rcware and influx
     :param since: UTC datetime to read data from
+    :param to: UTC datetime to read data to. Default: utcnow()
     :return: List of dictionaries ready for insertion in influxdb
     """
     data = []
     now = datetime.utcnow()
 
-    for rcw_meas in rcw_mapping:
+    for rcw_msrmt in rcw_mapping:
         # Make key value pairs for GetData
-        plant = factory.KeyValuePair(Key='StationName', Value=rcw_meas[0])
-        uuid = factory.KeyValuePair(Key='DPGuid', Value=rcw_meas[1])
+        plant = factory.KeyValuePair(Key='StationName', Value=rcw_msrmt[0])
+        uuid = factory.KeyValuePair(Key='DPGuid', Value=rcw_msrmt[1])
         aakvp = factory.ArrayOfArrayOfKeyValuePair(factory.ArrayOfKeyValuePair([plant, uuid]))
 
         val_offset = 0
@@ -84,19 +87,23 @@ def read_rcware_measurements(rcw_mapping: dict, since: datetime, to=datetime.utc
                 variableOffset=0,
                 variableCount=1,
                 valueOffset=val_offset,
-                valueCount=RCW_VARIABLE_COUNT,
+                valueCount=RCW_GETDATA_VALUE_COUNT,
             )
 
             if res['returnCode'] != "0;OK;":
                 print(res['returnCode'], plant, uuid)
                 break
 
-            for mvr in res['GetDataResult']['Mvr']:
-                for dp in rcw_to_influx(mvr, rcw_mapping):
-                    data.append(dp)
+            try:
+                for mvr in res['GetDataResult']['Mvr']:
+                    for dp in rcw_to_influx(mvr, rcw_mapping):
+                        data.append(dp)
+            except Exception:
+                print(res, rcw_msrmt)
+                break
 
             val_offset = res['nextValueOffset']
-            if val_offset == -1:  # no more values to read
+            if val_offset == -1:  # no more values to read for current variable
                 break
     return data
 
@@ -110,7 +117,11 @@ if __name__ == '__main__':
     while True:
         start = time.time()
         data = read_rcware_measurements(config, datetime.utcnow() - timedelta(minutes=5))
-        print(len(data), "dps done in", time.time() - start)
+        print(int(start), len(data), "dps done in", f'{time.time() - start:.2}')
         # print(data)
 
-        time.sleep(300)
+        influxdb.create_database('rcware_test')
+        influxdb.switch_database('rcware_test')
+        influxdb.write_points(data)
+
+        time.sleep(300-(time.time()-start))
