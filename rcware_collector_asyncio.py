356550fe453d2bbb452f5f6a9d85c936492f8d59
Teodor Yantcheff <teodor.yantchev@gmail.com>
2020-05-14 23:36:52 +0300

    save progress

--- rcware_collector_asyncio.py
+++ rcware_collector_asyncio.py
@@ -3,6 +3,7 @@ import csv
 import time
 from datetime import datetime, timedelta
 
+import backoff
 import zeep
 from zeep.asyncio import AsyncTransport
 from influxdb import InfluxDBClient
@@ -20,7 +21,7 @@ loop = asyncio.get_event_loop()
 loop.set_debug(False)
 
 settings = zeep.Settings(strict=True, xml_huge_tree=True, force_https=False, raw_response=False)
-transport = AsyncTransport(loop, cache=None)  # todo remove no cache
+transport = AsyncTransport(loop)
 # Available ports are HistoryAccess, HistoryAccessGZip, HistoryAccess1. Only HistoryAccess seems to be alive.
 client = zeep.Client(wsdl=WSDL, settings=settings, port_name='HistoryAccess', transport=transport)
 factory = client.type_factory('http://schemas.datacontract.org/2004/07/ESG.Db.Server.Shared')
@@ -44,34 +45,45 @@ rcw_mapping = load_mapping('rcware_collector-config.csv')
 
 
 def rcw_to_influx(mvr):
-    # (StationName: DPGuid) tuple used as key in the config dictionary
+    # (StationName, DPGuid) tuple used as key in the config dictionary
     lookup = (mvr['Keys']['KeyValuePair'][5]['Value'], mvr['Keys']['KeyValuePair'][1]['Value'])
-    # lookup = tuple([k['Value'] for k in mvr['Keys']['KeyValuePair'] if k['Key'] in ['DPGuid', 'StationName']])  # todo this can be indices
+    # lookup = tuple([k['Value'] for k in mvr['Keys']['KeyValuePair'] if k['Key'] in ['DPGuid', 'StationName']])
 
     datapoints = []
 
-    if not mvr['Vals']:
-        raise Exception('no values')
+    # if not mvr['Vals']:
+    #     raise ValueError('no values')
+    try:
+        for value in mvr['Vals']['I']:
+            ts = value['Gt']
+            val = value['Dv']
 
-    for value in mvr['Vals']['I']:
-        ts = value['Gt']
-        val = value['Dv']
+            dp_dict = rcw_mapping[lookup]
 
-        dp_dict = rcw_mapping[lookup]
-
-        dp = {
-            "measurement": dp_dict['measurement'],
-            "tags": {tag: dp_dict[tag] for tag in ["device", "plant", "device_type"]},
-            "time": ts,
-            "fields": {
-                "value": float(val)
-            }
-        }
-        datapoints.append(dp)
+            datapoints.append({
+                "measurement": dp_dict['measurement'],
+                "tags": {tag: dp_dict[tag] for tag in ["device", "plant", "device_type"]},
+                "time": ts,
+                "fields": {
+                    "value": float(val)
+                }
+            })
+    except IndexError:  # No values in the mvr
+        pass
     return datapoints
 
 
-async def read_single_measurement(semaphore, rcw_measurement, since: datetime, to):
+def backoff_hdlr(details):
+    print("Backing off {wait:0.1f} seconds afters {tries} tries "
+          "calling function {target} with args {args} and kwargs "
+          "{kwargs}".format(**details))
+
+
+@backoff.on_exception(backoff.expo,
+                      (Exception,),
+                      max_time=300,
+                      on_backoff=backoff_hdlr)
+async def read_single_measurement(semaphore, rcw_measurement, since: datetime, to: datetime):
     async with semaphore:
         # Make key value pairs for GetData
         plant = factory.KeyValuePair(Key='StationName', Value=rcw_measurement[0])
@@ -95,15 +107,11 @@ async def read_single_measurement(semaphore, rcw_measurement, since: datetime, t
 
             if res['returnCode'] != "0;OK;":
                 print(plant, uuid, res['returnCode'])
-                break
-
-            try:
-                for mvr in res['GetDataResult']['Mvr']:
-                    for dp in rcw_to_influx(mvr):
-                        data.append(dp)
-            except Exception:
-                print(rcw_measurement, res)
                 raise
+
+            for mvr in res['GetDataResult']['Mvr']:
+                for dp in rcw_to_influx(mvr):
+                    data.append(dp)
 
             val_offset = res['nextValueOffset']
             if val_offset == -1:  # no more values to read for current variable
@@ -111,15 +119,16 @@ async def read_single_measurement(semaphore, rcw_measurement, since: datetime, t
         return data
 
 
-async def read_rcware_measurements(since: datetime, to=datetime.utcnow()):
+async def read_rcware_measurements(since: datetime, to: datetime):
     """
 
     :param since: UTC datetime to read data from
-    :param to: UTC datetime to read data to. Default: utcnow()
     :return: List of dictionaries ready for insertion in influxdb
     """
     semaphore = asyncio.Semaphore(MAX_CONCURRENCY)
     futures = []
+
+    print(since.strftime('%H:%M:%S'), ">", to.strftime('%H:%M:%S'))
 
     for rcw_msrmt in rcw_mapping.keys():
         futures.append(asyncio.create_task(read_single_measurement(semaphore, rcw_msrmt, since, to)))
@@ -132,18 +141,23 @@ if __name__ == '__main__':
 
     while True:
         start = time.time()
-        results = loop.run_until_complete(read_rcware_measurements(datetime.utcnow() - timedelta(minutes=5)))
+        now = datetime.utcnow()
+        since = now-timedelta(minutes=7)
+        # since = now-timedelta(days=14)
+        results = loop.run_until_complete(read_rcware_measurements(since=since, to=now))
         exceptions = [r for r in results if isinstance(r, Exception)]
-        if exceptions:
-            print(exceptions[0])
-        print(int(start), len(results), "dps done in", f'{time.time() - start:.2f}s', f'exceptions: {len(exceptions)}')
+        # if exceptions:
+        #     print(exceptions[0])
+
         # print(results)
-        data = [r[0] for r in results if not isinstance(r, Exception)]
-
-        influxdb.create_database('rcware_test')
-        influxdb.switch_database('rcware_test')
-        influxdb.write_points(data)
-
+        # data = [r[0] for r in results if not isinstance(r, Exception)]
+        data = [dp for r in results if not isinstance(r, Exception) for dp in r]
+        print(int(start), len(data), "dps done in", f'{time.time() - start:.2f}s', f'exceptions: {len(exceptions)}')
+        print('writing to influx')
+        influxdb.create_database('sdb')
+        influxdb.switch_database('sdb')
+        influxdb.write_points(data, batch_size=20000)
+        print('influx done')
         time.sleep(300 - (time.time() - start))
 
     # loop.run_until_complete(transport.session.close())
